65a682a401d479e8f3157ad5a0eabf646dd6dd2d
Nicci Sheets <niccisheets@aol.com>
2018-06-06 14:48:52 -0400

    added comments detailing what each line of code does

--- print_to_screen.rb
+++ print_to_screen.rb
@@ -14,18 +14,18 @@
 
 
 number = 1
-100.times do
-	if number % 15 == 0
-		puts "mined minds"
-	elsif number % 3 == 0    
+100.times do # does the following function 100 times
+	if number % 15 == 0 # replaces multiples of 15 with mined minds
+		puts "mined minds" 
+	elsif number % 3 == 0 # replaces multiples of 3 with mined
 		puts "mined"
-	elsif number % 5 == 0
+	elsif number % 5 == 0 # replaces multiples of 5 with minds
 		puts "minds"
 	else
-		puts number
-	end 
-	number = number+1
-end
+		puts number # numbers that are not a multiple of 15, 3 or 5 receive numerical value
+	end # ends function
+	number = number+1 # increases each numerical value by +1
+end # ends function
 
 
 
