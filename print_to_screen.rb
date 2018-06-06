c91cdd9dbea3a130d0fab1748a5278f33b96fac8
Nicci Sheets <niccisheets@aol.com>
2018-06-06 13:52:15 -0400

    replaced 3s with mined and replaced 5s with minds

--- print_to_screen.rb
+++ print_to_screen.rb
@@ -15,14 +15,15 @@
 
 number = 1
 100.times do
-	if number == 3
+	if number % 3 == 0    
 		puts "mined"
+	elsif number % 5 == 0
+		puts "minds"
 	else
-	puts number
+		puts number
 	end 
 	number = number+1
 end
-
 
 
 
